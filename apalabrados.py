7fbdf97d215e1a43697650196e3568bcda770416
Mauricio Bautista <Mauricio.Bautista@dentsu.com>
2021-10-14 23:47:49 -0500

    se agrego la logica que obtiene el primer y ultimo caracter en el texto

--- apalabrados.py
+++ apalabrados.py
@@ -1,14 +1,18 @@
+from typing import final
 import db_connection
 
-def numero(num: float)-> None:
+
+def numero(num: float) -> None:
     pass
 
 
-def texto(string: str)-> None:
-    pass
+def texto(string: str) -> None:
+    arr = list(string)
+    initial = arr[0]
+    final = arr[len(string)-1]
 
 
-def numero(character: str)-> None:
+def character(char_list: list) ->None:
     pass
 
 
@@ -20,10 +24,31 @@ def is_num(value):
         return False
 
 
+def contains_special(input_v):
+    specials = []
+    arr = list(input_v)
+    char = ''
+    for x in range(len(input_v)):
+        if arr[x] == '_':
+            char = arr[x]
+            print(arr[x])
+            specials.append(char)
+    return specials
+
+
 def eveluate():
-    value = input("Digite algo")
-    print(is_num(value))
+    input_v = input("Digite algo: ")
+    if is_num(input_v):
+        numero()
+    else:
+        specials = contains_special(input_v)
+        if len(specials) > 0:
+            character(specials)
+        texto(input_v)
+        
+        
+
+
 
 
 eveluate()
-
